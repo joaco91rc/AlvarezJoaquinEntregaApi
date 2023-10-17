acc61959cae6257441e5b1534974828843b619ba
Joaco Alvarez <joaquinaalvarez91@gmail.com>
2023-10-16 23:40:19 -0300

    Entrega Api

---
+++ CapaDatosEFCore/SistemaGestionContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CapaEntidad;
+
+namespace CapaDatosEFCore
+{
+    public  class SistemaGestionContext:DbContext
+    {
+        public DbSet<Producto> Productos { get; set; }
+        public DbSet<ProductoVendido> ProductosVendidos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Venta> Ventas { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=SistemaGestionEF;Trusted_Connection=true");
+        }
+    }
+}
---
+++ SistemaGestionWebApi/Controllers/UsuariosController.cs
@@ -0,0 +1,27 @@
+﻿using CapaEntidad;
+using CapaNegocio;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using CapaDatos;
+using System.Configuration;
+
+namespace SistemaGestionWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        [HttpGet(Name = "GetUsuarios") ]
+        public IEnumerable<Usuario> GetUsuarios()
+        {
+            
+
+                // Aquí puedes utilizar la cadena de conexión y consultar la lista de usuarios
+                List<Usuario> usuarios = new CN_Usuario().ListarUsuarios();
+
+                return usuarios.ToArray();
+            
+            
+        }
+    }
+}
